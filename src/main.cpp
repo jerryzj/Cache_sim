cfa76dcf7c077337b21de8d9d9389d3360ee2cd1
jerryzj <jerry0715no14@gmail.com>
2019-09-03 15:47:51 +0800

    Use argparse lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
+#include "argparse.hpp"
 #include "cache.hpp"
 
 int main(int argc, char *argv[]) {
-    assert(argc >= 3);
+    ArgumentParser parser("Argument parser");
+    parser.add_argument("-t", "Trace file");
+    parser.add_argument("-c", "Cache config file");
+    try {
+        parser.parse(argc, argv);
+    } catch (const ArgumentParser::ArgumentNotFound &ex) {
+        std::cerr << ex.what() << std::endl;
+        return -1;
+    }
+    if (parser.is_help())
+        return 0;
+    std::string config = parser.get<std::string>("c");
+    std::string trace = parser.get<std::string>("t");
 
-    Cache cache(argv[2]);
-    cache.run_sim(argv[1]);
-    cache.dump_result(argv[1]);
+    Cache cache(config.c_str());
+    cache.run_sim(trace.c_str());
+    cache.dump_result(trace.c_str());
 
     return 0;
 }
