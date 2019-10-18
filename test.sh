43833cf031cbde7984a0be56e7a7c238531be666
jerryzj <jerry0715no14@gmail.com>
2019-10-19 00:26:44 +0800

    Add full tests

--- test.sh
+++ test.sh
@@ -3,5 +3,19 @@ cd build
 cmake ..
 make -j
 ./cache_sim -t ../TestData/gcc.trace -c ../TestData/cache1.cfg
+./cache_sim -t ../TestData/gzip.trace -c ../TestData/cache1.cfg
+./cache_sim -t ../TestData/mcf.trace -c ../TestData/cache1.cfg
+./cache_sim -t ../TestData/swim.trace -c ../TestData/cache1.cfg
+./cache_sim -t ../TestData/twolf.trace -c ../TestData/cache1.cfg
+
 ./cache_sim -t ../TestData/gcc.trace -c ../TestData/cache2.cfg
+./cache_sim -t ../TestData/gzip.trace -c ../TestData/cache2.cfg
+./cache_sim -t ../TestData/mcf.trace -c ../TestData/cache2.cfg
+./cache_sim -t ../TestData/swim.trace -c ../TestData/cache2.cfg
+./cache_sim -t ../TestData/twolf.trace -c ../TestData/cache2.cfg
+
 ./cache_sim -t ../TestData/gcc.trace -c ../TestData/cache3.cfg
+./cache_sim -t ../TestData/gzip.trace -c ../TestData/cache3.cfg
+./cache_sim -t ../TestData/mcf.trace -c ../TestData/cache3.cfg
+./cache_sim -t ../TestData/swim.trace -c ../TestData/cache3.cfg
+./cache_sim -t ../TestData/twolf.trace -c ../TestData/cache3.cfg
