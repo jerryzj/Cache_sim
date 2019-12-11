ec7d4e8f8cadb32048e4daf653b90f604fbe8073
jerryzj <jerry0715no14@gmail.com>
2019-12-11 12:23:54 +0800

    [Bug] Exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,17 @@
 
 int simulator_verbose_output = 0;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
     ArgumentParser parser("Argument parser");
     parser.add_argument("-t", "Program trace file");
     parser.add_argument("-c", "Cache config file");
     parser.add_argument("-v", "--verbose", "Verbose output", false);
 
+    // BUG: Exception not handled successfully
     try {
         parser.parse(argc, argv);
     } catch (const ArgumentParser::ArgumentNotFound &ex) {
-        std::cerr << ex.what() << std::endl;
-        return -1;
-    }
-
-    if (!parser.exists("t") || !parser.exists("c")) {
+        //std::cerr << ex.what() << std::endl;
         parser.print_help();
         return -1;
     }
