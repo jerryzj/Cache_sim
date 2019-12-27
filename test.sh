9c8df35606146f58bbd9b0f8b79153f78dd62d16
jerryzj <jerry0715no14@gmail.com>
2019-12-27 22:53:24 +0800

    Add multi-layer cache tests

--- test.sh
+++ test.sh
@@ -19,3 +19,9 @@ make -j
 ./cache_sim -t ../TestData/mcf.trace -c ../TestData/cache3.json
 ./cache_sim -t ../TestData/swim.trace -c ../TestData/cache3.json
 ./cache_sim -t ../TestData/twolf.trace -c ../TestData/cache3.json
+
+./cache_sim -t ../TestData/gcc.trace -c ../TestData/cache4.json
+./cache_sim -t ../TestData/gzip.trace -c ../TestData/cache4.json
+./cache_sim -t ../TestData/mcf.trace -c ../TestData/cache4.json
+./cache_sim -t ../TestData/swim.trace -c ../TestData/cache4.json
+./cache_sim -t ../TestData/twolf.trace -c ../TestData/cache4.json
