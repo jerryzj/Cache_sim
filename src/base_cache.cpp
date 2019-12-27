73d4899c5865d4d4b8638de5f13c2959e7da536f
Jacky Huang <jackyhuang85@gmail.com>
2019-12-27 18:19:44 +0800

    fix coding style

--- src/base_cache.cpp
+++ src/base_cache.cpp
@@ -1,7 +1,6 @@
 #include "base_cache.hpp"
 
-BaseCache::BaseCache(const CachePropertyStruct &setting) {
-    property = setting;
+BaseCache::BaseCache(const CachePropertyStruct &setting) : property(setting) {
 
     // set cache block size/bit
     property._bit_offset = log2l(setting._block_size - 1);
